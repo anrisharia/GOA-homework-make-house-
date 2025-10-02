7e0bbef10b919b7e929ce880b2569ba6ec5078f4
anrisharia <anrisharia5@gmail.com>
2025-10-02 20:10:47 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,46 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1: draw a square
+speed(30)
+width(7)
+color("blue")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+#end of square
+
+#drawing the door
+
+forward(70)
+color("green")
+left(90)
+forward(120)   #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+exitonclick()

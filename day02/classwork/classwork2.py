e56b2d23de0d0a4ba16a19fb7abda5f7c0fe253b
anrisharia <anrisharia5@gmail.com>
2025-10-05 18:58:24 +0400

    commit

---
+++ day02/classwork/classwork2.py
@@ -0,0 +1,29 @@
+# 1 classwork
+# string (str) -> სტრინგი არის ნებისმიერი მონაცემი რომელიც მოქცეულია ბრჭყალებში
+name = "anri"
+print(name)
+
+
+# integer (int) -> ინტეჟერი არის ნებისმიერი მთელი რიცხვი
+age = 20
+print(age)
+
+
+# float -> არის ნებისმიერი ათწილადი რიცხვი
+number = 5.5
+print(number)
+
+
+#boolean (bool) -> ორი შესაძლო მნიშვნელობიდან ერთ-ერთი True ან False
+print(10 > 9)
+print(10 < 9)
+
+# 2 classwork
+age = 14
+print(age)
+age = 14
+print(age + 87)
+
+
+# 3 classwork 
+# ერორი ისაა რომ პრინტის დროს ცვლადი უნდა გამოვიყენოთ და არა მნიშვნელობა.
---
+++ day02/homework/homework2.py
@@ -0,0 +1,49 @@
+# 1 homework
+
+name = "anri"
+print(name)
+
+LastName = "sharia"
+print(LastName)
+
+# 2 homework
+
+# integer
+age = 23
+print(age)
+
+# string
+name = "irakli"
+print(name)
+
+# float
+number = 7.7
+print(number)
+
+# boolean
+print(2 > 1)
+print(2 < 1)
+
+# 3 homework
+
+#1
+age = 14
+print(age)
+
+#2
+age = 16
+print(age)
+
+#3
+age = 42
+print(age)
+
+#4
+age = 47
+print(age)
+
+#5
+age = 78
+print(age)
+
+print(14 + 16 + 42 + 47 + 78)

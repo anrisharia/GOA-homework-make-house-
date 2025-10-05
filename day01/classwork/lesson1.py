6f7bc365e17556a3735b602ebbda0ad7a69e12fa
anrisharia <anrisharia5@gmail.com>
2025-10-05 17:22:04 +0400

    commit

---
+++ day01/classwork/lesson1.py
@@ -0,0 +1,14 @@
+name = "anri" 
+
+print(name)
+
+
+surname = "sharia" 
+
+print(surname)
+
+print(155+145)
+
+print(290-160)
+
+print(20*15)
---
+++ day01/homework/homework1.py
@@ -0,0 +1,28 @@
+#homework 2
+print("English")
+
+# homework 3
+print("Rambo: last blood")
+
+# homework 4
+print("anri")
+print("ani")
+print("tea")
+print("demna")
+print("medea")
+
+#home work 5
+name = "anri"
+print(name)
+
+name = "ani"
+print(name)
+
+name = "tea"
+print(name)
+
+name = "demna"
+print(name)
+
+name = "medea"
+print(name)

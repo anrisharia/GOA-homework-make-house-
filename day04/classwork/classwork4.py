a0e8232730fe32be6a5c25349f9055d35913693d
anrisharia <anrisharia5@gmail.com>
2025-10-07 22:47:43 +0400

    commit

---
+++ day04/classwork/classwork4.py
@@ -0,0 +1,34 @@
+# 1 classwork
+
+# ჩვენ გვაქვს 1. ტექსტური მონაცემთა ტიპი. 2. რიცხვული მონაცემთა ტიპი ესენია: 1) სტრიგნი. 2) ინტეჟერი, ფლოუტი და ბოლეანი.
+
+#1
+#string
+name = "anri"
+print(name)
+
+#2
+#integer
+age = 14
+print(age)
+
+#3
+#float
+number = 5.6
+print(number)
+
+#4
+#boolean
+print(3>2)
+# 2 classwork
+
+brand = "nike"
+brand = "puma"
+brand = "adidas"
+print(brand)
+
+# 3 classwork
+number2 = 20
+number3 = 4
+equals = number2*number3
+print(equals)

0ecea82016d7255b7af45ecfe9cffdf4f089c227
anrisharia <anrisharia5@gmail.com>
2025-10-12 22:40:18 +0400

    comiit

---
+++ day06/classwork/classwork6.py
@@ -0,0 +1,13 @@
+# 1 classwork
+# snake_case, Camelcase და PascalCase არის სტრუქტურული ქეისი
+
+# 2 classwork 
+
+#input ->  არის შემავალი ინფორმაცია.
+#output ->  არის გამომავალი ინფორმაცია.
+
+# 3 classwork
+
+age = input("enter your age:")
+
+my_age = 6

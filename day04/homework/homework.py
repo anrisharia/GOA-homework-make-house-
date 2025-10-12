d3982357a3def257c297908a2f18e87898e8a167
anrisharia <anrisharia5@gmail.com>
2025-10-12 20:06:56 +0400

    commit

---
+++ day04/homework/homework.py
@@ -0,0 +1,28 @@
+# 3 homework
+
+number = 7
+age = 14
+result = number*age
+print(result)
+
+# 4 homework
+
+# 1
+number1 = 40
+number2 = 50
+print(number1 + number2)
+
+# 2
+number3 = 140
+number4 = 70
+print(number3 - number4)
+
+# 3 
+number5 = 300
+number6 = 30
+print(number5 / number6)
+
+# 4 
+number7 = 15
+number8 = 10
+print(number7*number8)
---
+++ day04/homework/homework4.py
@@ -0,0 +1,52 @@
+# 1 homework
+
+points = 50
+points = 20
+points = 14
+print(points)
+
+# 2 homework
+
+# 1
+age = "Anri" 
+age = 14 
+# 2
+Name = 15
+Name = "Anri"
+# 3
+number = "14" 
+number = 14
+# 4
+brand = adidas
+brand = "adidas" 
+# 5 
+decimal = 5
+decimal = 5.5
+# 6 
+movie = "Prison Break"
+print("Prison Break")
+print(movie)
+# 7
+print(band)
+band = "rock"
+band = "rock"
+print(band)
+# 8 
+number1 = 4
+text = "hello"
+print(number1 + text)
+number = "4"
+text = "hello"
+print(number1 + text)
+# 9 
+print(Nikolozi)
+# 10
+print("5")
+
+
+
+
+
+
+
+
